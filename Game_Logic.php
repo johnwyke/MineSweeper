b3a5b10442b88194f5d2ad14967d2c263161a966
johnwyke <johnwyke@mail.weber.edu>
2019-05-30 07:17:03 -0600

    Game Board Is finished

--- Game_Logic.php
+++ Game_Logic.php
@@ -24,6 +24,11 @@
 				public function set_mine($bool){
 				$this->mine = $bool;
 				}
+				
+				public function inc_value(){
+					$this->value++;
+				}
+				public function get_value(){return $this->value;}
 			}
 			
 			
@@ -32,20 +37,20 @@
 			//*******************
 			
 			// Vars 
-			$numbOfMines =10;
+			$numbOfMines = 10;
 			$colCount = 15;
 			$rowCount = 15;
 			
 			// Init game board
-			for($r =0;  $r <$rowCount-1; $r++){
-				for($c=0; $c<$colCount-1; $c++){
+			for($r =0;  $r <$rowCount; $r++){
+				for($c=0; $c<$colCount; $c++){
 					$gameBoard[$r][$c] = new cell();
 				}// End of Inner for 			
 			}// End of outer for 
 			
 			// Fill the Game Board with Mines 
-						
-			for($r =0;  $r < $numbOfMines; $r++){
+				$mc =0;		
+			while($mc < $numbOfMines){
 				
 				$xCord = rand(0,$colCount-1);
 				$yCord = rand(0,$rowCount-1);
@@ -53,55 +58,187 @@
 				//TODO:
 				// Need to see if mine is already present
 				// If it is start loop again 
-				$gameBoard[$yCord][$xCord] = new cell();
+				
+				
+				if($gameBoard[$yCord][$xCord]->get_mine()){
+					echo "I am at a cell with a mine already";
+					// Mine is already set try agian.
+					$mc--;
+					continue;
+				}			
 				
 				echo "Placing mine ";
 				$gameBoard[$yCord][$xCord]->set_mine(true); 
 				
 				echo "I am at position Row: " . $yCord ." Col: ".$xCord . "<br>";
-				
+				$mc++;
 			}// End of outer for 
 			
 			
 			// TODO: Check neighbors for mines Then Set value
 			
-			// Check same row
 			for($r=0; $r<$rowCount-1; $r++){
 				for($c=0; $c<$colCount-1; $c++){
+					
+					// If I am a mine Move to next
+					if($gameBoard[$r][$c]->get_mine()){
+						continue;
+					}
+					
 					if($r==0){
 						// Skip checking Top
-						if($c ==0){
-							// First in Row Only Check right
-						}
-						else if	($c == $colCount-1){
-							//Last in row only check left
+							if($c ==0){
+								// First in Col Only Check right
+								if ($gameBoard[$r][$c+1]->get_mine()){
+									$gameBoard[$r][$c]->inc_value();
+								}
+								if ($gameBoard[$r+1][$c]->get_mine()){
+									$gameBoard[$r][$c]->inc_value();
+								}
+								if ($gameBoard[$r+1][$c+1]->get_mine()){
+									$gameBoard[$r][$c]->inc_value();
+								}
+							}elseif ($c == $colCount-1){
+								//Last in Col only check left
+								if ($gameBoard[$r][$c-1]->get_mine()){
+									$gameBoard[$r][$c]->inc_value();
+								}
+								if ($gameBoard[$r+1][$c]->get_mine()){
+									$gameBoard[$r][$c]->inc_value();
+								}
+								if ($gameBoard[$r+1][$c-1]->get_mine()){
+									$gameBoard[$r][$c]->inc_value();
+								}
+							}elseif($c!=0 && $c != $colCount-1){
+								
+								// Check Both
+								if ($gameBoard[$r][$c+1]->get_mine()){
+									$gameBoard[$r][$c]->inc_value();
+								}
+								if ($gameBoard[$r][$c-1]->get_mine()){
+									$gameBoard[$r][$c]->inc_value();
+								}
+								if ($gameBoard[$r+1][$c+1]->get_mine()){
+									$gameBoard[$r][$c]->inc_value();
+								}
+								if ($gameBoard[$r+1][$c-1]->get_mine()){
+									$gameBoard[$r][$c]->inc_value();
+								}
+								if ($gameBoard[$r+1][$c]->get_mine()){
+									$gameBoard[$r][$c]->inc_value();
+								}
+							}
 							
-						}else {
-							// Check Both
-						}
-					}else if ($r == $rowCount-1){
-						// Skipcheck bottom
-						if($c ==0){
-							// First in Row Only Check right
-						}
-						else if	($c == $colCount-1){
-							//Last in row only check left
 							
-						}else {
-							// Check Both
-						}
-					}else{
-						//Check all three
-						if($c ==0){
-							// First in Row Only Check right
-						}
-						else if	($c == $colCount-1){
-							//Last in row only check left
-							
-						}else {
-							// Check Both
-						}
-					}
+						}elseif ($r == $rowCount-1){
+							// Skipcheck bottom
+							if($c ==0){
+								// First in Col Only Check right
+								if ($gameBoard[$r][$c+1]->get_mine()){
+									$gameBoard[$r][$c]->inc_value();
+								}
+								if ($gameBoard[$r-1][$c]->get_mine()){
+									$gameBoard[$r][$c]->inc_value();
+								}
+								if ($gameBoard[$r-1][$c+1]->get_mine()){
+									$gameBoard[$r][$c]->inc_value();
+								}
+							}elseif ($c == $colCount-1){
+								//Last in Col only check left
+								if ($gameBoard[$r][$c-1]->get_mine()){
+									$gameBoard[$r][$c]->inc_value();
+								}
+								if ($gameBoard[$r-1][$c]->get_mine()){
+									$gameBoard[$r][$c]->inc_value();
+								}
+								if ($gameBoard[$r-1][$c-1]->get_mine()){
+									$gameBoard[$r][$c]->inc_value();
+								}
+							}elseif($c!=0 && $c != $colCount-1){
+								
+								// Check Both
+								if ($gameBoard[$r][$c+1]->get_mine()){
+									$gameBoard[$r][$c]->inc_value();
+								}
+								if ($gameBoard[$r][$c-1]->get_mine()){
+									$gameBoard[$r][$c]->inc_value();
+								}
+								if ($gameBoard[$r-1][$c+1]->get_mine()){
+									$gameBoard[$r][$c]->inc_value();
+								}
+								if ($gameBoard[$r-1][$c-1]->get_mine()){
+									$gameBoard[$r][$c]->inc_value();
+								}
+								if ($gameBoard[$r-1][$c]->get_mine()){
+									$gameBoard[$r][$c]->inc_value();
+								}
+							}
+						}else{
+							//Check all three
+							if($c ==0){
+								// First Col 
+								if ($gameBoard[$r][$c+1]->get_mine()){
+									$gameBoard[$r][$c]->inc_value();
+								}
+								if ($gameBoard[$r-1][$c]->get_mine()){
+									$gameBoard[$r][$c]->inc_value();
+								}
+								if ($gameBoard[$r-1][$c+1]->get_mine()){
+									$gameBoard[$r][$c]->inc_value();
+								}
+								if ($gameBoard[$r+1][$c+1]->get_mine()){
+									$gameBoard[$r][$c]->inc_value();
+								}
+								if ($gameBoard[$r+1][$c]->get_mine()){
+									$gameBoard[$r][$c]->inc_value();
+								}
+							}elseif ($c == $colCount-1){
+								//Last in Col only check left
+								if ($gameBoard[$r][$c-1]->get_mine()){
+									$gameBoard[$r][$c]->inc_value();
+								}
+								if ($gameBoard[$r-1][$c]->get_mine()){
+									$gameBoard[$r][$c]->inc_value();
+								}
+								if ($gameBoard[$r-1][$c-1]->get_mine()){
+									$gameBoard[$r][$c]->inc_value();
+								}
+								if ($gameBoard[$r+1][$c]->get_mine()){
+									$gameBoard[$r][$c]->inc_value();
+								}
+								if ($gameBoard[$r+1][$c-1]->get_mine()){
+									$gameBoard[$r][$c]->inc_value();
+								}
+							}elseif($c!=0 && $c != $colCount-1){
+								
+								// Check Both
+								if ($gameBoard[$r][$c+1]->get_mine()){
+									$gameBoard[$r][$c]->inc_value();
+								}
+								if ($gameBoard[$r][$c-1]->get_mine()){
+									$gameBoard[$r][$c]->inc_value();
+								}
+								if ($gameBoard[$r-1][$c+1]->get_mine()){
+									$gameBoard[$r][$c]->inc_value();
+								}
+								if ($gameBoard[$r-1][$c-1]->get_mine()){
+									$gameBoard[$r][$c]->inc_value();
+								}
+								if ($gameBoard[$r-1][$c]->get_mine()){
+									$gameBoard[$r][$c]->inc_value();
+								}
+								if ($gameBoard[$r+1][$c]->get_mine()){
+									$gameBoard[$r][$c]->inc_value();
+								}
+								if ($gameBoard[$r+1][$c+1]->get_mine()){
+									$gameBoard[$r][$c]->inc_value();
+								}
+								if ($gameBoard[$r+1][$c-1]->get_mine()){
+									$gameBoard[$r][$c]->inc_value();
+								}
+								
+							}
+						}									
 				}// End Inner 
 			}// End Outer 
 			
@@ -112,11 +249,13 @@
 					//var_dump($gameBoard[$r][$c]->get_mine());
 					if ($gameBoard[$r][$c]->get_mine()){
 						echo " I have a mine at cell Row: " .$r . " Col: ". $c . "<br>"; 
+					}elseif($gameBoard[$r][$c]->get_value()!=0) {
+						echo "I have " . $gameBoard[$r][$c]->get_value() ." Mines Close to me <br>";
 					}
 				
-					//echo " cell Row: " .$r . " Col: ". $c . "<br>";
+					
 				}// End of Inner for 			
-			}// End of outer for 			
+			}// End of outer for 	
 		?>
 	</body>
 </html>
