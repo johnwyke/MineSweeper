73889d502163af26dba9110752b33454a260bb2b
juanrangeljuarez <juanrangel1@mail.weber.edu>
2019-06-24 14:14:55 -0700

    changes to make login work on minesweeper

--- playGame.php
+++ playGame.php
@@ -1,14 +1,15 @@
-<html>
+
+<!DOCTYPE html>
 	<head>
-		
-		<title>Minessweeper</title>
+			<title>Minesweeper</title>
 		
 	</head>
-	<body>
+	<body onload='timer=setInterval("countUP()", 1000 );'>
 		<form action="#" method="post">
 			<button type ="submit" name="logout" value="send to database">Logout</button>			
     	</form>
-    	<input type ="button" name="stop" value="Stop Me" onclick="stopCounter();" />	
+    	<br/>	
+    	Time: <div id="timer_container"></div>
 		<p>
 			<script type="text/javascript">
 			function clearTimer()
@@ -18,318 +19,7 @@
 				 }
 				  
 			</script>
-		<?php
-			session_start();
-			$user = $_SESSION["user_name"];
-			echo "Welcome ".$user;
-
-			if (isset($_POST['logout']))
-        	{
-        			session_start();
-
-        			echo '<script type="text/javascript">',
-     					'clearTimer();',
-    					 '</script>';
-        			/*echo "<script> window.onload = function() {
-     						stopCounter();
- 						}; </script>";*/
-					$_SESSION["user_name"] = "";
-					unset($_SESSION['user_name']);
-					header("Refresh: 1; url = http://cs3750juan.epizy.com/login.php");		             	
-			}
-	
-			if(isset($_POST['postcounter']))
-			{
-				$time = $_POST['postcounter'];
-				$user = $_SESSION["user_name"];
-				
-				$servername = "sql304.epizy.com";
-				$username = "epiz_23868833";
-				$password = "oZwNrrIhSLY3r";
-				$dbname =  "epiz_23868833_game";
-
-				$conn = new mysqli($servername, $username,$password,$dbname);
-
-				if($conn->connect_error)
-				{
-					die("Connection failed:". $conn->connect_error);
-				}
-				$sql = "SELECT * FROM  GameMines WHERE User = '" . $user . "'";
-				$result = $conn->query($sql);
-				if($result->num_rows > 0)
-				{
-					$row = $result->fetch_assoc();
-					$idUser = $row['ID'];
-					$query = "INSERT INTO Scores (IDName, Score) VALUES ('" . $idUser . "', '" . $time . "')"; 
-					mysqli_query($conn,$query);
-					echo $idUser." ".$time;
-					session_start();
-					$_SESSION["user_name"] = $user;
-				}
-
-			}
-		?>
-		</p>
-		<?php
-			session_start();
-			class cell{
-				private $mine; 
-				private $value; 
-				
-				// Constructor
-				public function __construct(){
-					$mine = false;
-					$value = 0;
-				}
-				// function to return mine or not
-				public function get_mine(){
-						if ($this->mine == true){
-							return true; 
-						}
-						return false;
-					
-				}
-				public function set_mine($bool){
-				$this->mine = $bool;
-				}
-				
-				public function inc_value(){
-					$this->value++;
-				}
-				public function get_value(){return $this->value;}
-			}
-			
-			
-			//*******************
-			// Start of Game Logic 
-			//*******************
-			
-			// Vars 
-			$numbOfMines = 15;
-			$colCount = 15;
-			$rowCount = 15;
-			
-			// Init game board
-			for($r =0;  $r <$rowCount; $r++){
-				for($c=0; $c<$colCount; $c++){
-					$gameBoard[$r][$c] = new cell();
-						
-				}// End of Inner for 			
-			}// End of outer for 
-			
-			// Fill the Game Board with Mines 
-				$mc =0;		
-			while($mc < $numbOfMines){
-				
-				$xCord = rand(0,$colCount-2);
-				$yCord = rand(0,$rowCount-2);
-				
-				//TODO:
-				// Need to see if mine is already present
-				// If it is start loop again 
-				
-				
-				if($gameBoard[$yCord][$xCord]->get_mine()){
-					// Mine is already set try agian.
-					$mc--;
-					continue;
-				}			
-				
-				$gameBoard[$yCord][$xCord]->set_mine(true); 
-				
-				$mc++;
-			}// End of outer for 
-			
-			
-			// TODO: Check neighbors for mines Then Set value
-			
-			for($r=0; $r<$rowCount-1; $r++){
-				for($c=0; $c<$colCount-1; $c++){
-					
-					// If I am a mine Move to next
-					if($gameBoard[$r][$c]->get_mine()){
-						continue;
-					}
-					
-					if($r==0){
-						// Skip checking Top
-							if($c ==0){
-								// First in Col Only Check right
-								if ($gameBoard[$r][$c+1]->get_mine()){
-									$gameBoard[$r][$c]->inc_value();
-								}
-								if ($gameBoard[$r+1][$c]->get_mine()){
-									$gameBoard[$r][$c]->inc_value();
-								}
-								if ($gameBoard[$r+1][$c+1]->get_mine()){
-									$gameBoard[$r][$c]->inc_value();
-								}
-							}elseif ($c == $colCount-1){
-								//Last in Col only check left
-								if ($gameBoard[$r][$c-1]->get_mine()){
-									$gameBoard[$r][$c]->inc_value();
-								}
-								if ($gameBoard[$r+1][$c]->get_mine()){
-									$gameBoard[$r][$c]->inc_value();
-								}
-								if ($gameBoard[$r+1][$c-1]->get_mine()){
-									$gameBoard[$r][$c]->inc_value();
-								}
-							}elseif($c!=0 && $c != $colCount-1){
-								
-								// Check Both
-								if ($gameBoard[$r][$c+1]->get_mine()){
-									$gameBoard[$r][$c]->inc_value();
-								}
-								if ($gameBoard[$r][$c-1]->get_mine()){
-									$gameBoard[$r][$c]->inc_value();
-								}
-								if ($gameBoard[$r+1][$c+1]->get_mine()){
-									$gameBoard[$r][$c]->inc_value();
-								}
-								if ($gameBoard[$r+1][$c-1]->get_mine()){
-									$gameBoard[$r][$c]->inc_value();
-								}
-								if ($gameBoard[$r+1][$c]->get_mine()){
-									$gameBoard[$r][$c]->inc_value();
-								}
-							}
-							
-							
-						}elseif ($r == $rowCount-1){
-							// Skipcheck bottom
-							if($c ==0){
-								// First in Col Only Check right
-								if ($gameBoard[$r][$c+1]->get_mine()){
-									$gameBoard[$r][$c]->inc_value();
-								}
-								if ($gameBoard[$r-1][$c]->get_mine()){
-									$gameBoard[$r][$c]->inc_value();
-								}
-								if ($gameBoard[$r-1][$c+1]->get_mine()){
-									$gameBoard[$r][$c]->inc_value();
-								}
-							}elseif ($c == $colCount-1){
-								//Last in Col only check left
-								if ($gameBoard[$r][$c-1]->get_mine()){
-									$gameBoard[$r][$c]->inc_value();
-								}
-								if ($gameBoard[$r-1][$c]->get_mine()){
-									$gameBoard[$r][$c]->inc_value();
-								}
-								if ($gameBoard[$r-1][$c-1]->get_mine()){
-									$gameBoard[$r][$c]->inc_value();
-								}
-							}elseif($c!=0 && $c != $colCount-1){
-								
-								// Check Both
-								if ($gameBoard[$r][$c+1]->get_mine()){
-									$gameBoard[$r][$c]->inc_value();
-								}
-								if ($gameBoard[$r][$c-1]->get_mine()){
-									$gameBoard[$r][$c]->inc_value();
-								}
-								if ($gameBoard[$r-1][$c+1]->get_mine()){
-									$gameBoard[$r][$c]->inc_value();
-								}
-								if ($gameBoard[$r-1][$c-1]->get_mine()){
-									$gameBoard[$r][$c]->inc_value();
-								}
-								if ($gameBoard[$r-1][$c]->get_mine()){
-									$gameBoard[$r][$c]->inc_value();
-								}
-							}
-						}else{
-							//Check all three
-							if($c ==0){
-								// First Col 
-								if ($gameBoard[$r][$c+1]->get_mine()){
-									$gameBoard[$r][$c]->inc_value();
-								}
-								if ($gameBoard[$r-1][$c]->get_mine()){
-									$gameBoard[$r][$c]->inc_value();
-								}
-								if ($gameBoard[$r-1][$c+1]->get_mine()){
-									$gameBoard[$r][$c]->inc_value();
-								}
-								if ($gameBoard[$r+1][$c+1]->get_mine()){
-									$gameBoard[$r][$c]->inc_value();
-								}
-								if ($gameBoard[$r+1][$c]->get_mine()){
-									$gameBoard[$r][$c]->inc_value();
-								}
-							}elseif ($c == $colCount-1){
-								//Last in Col only check left
-								if ($gameBoard[$r][$c-1]->get_mine()){
-									$gameBoard[$r][$c]->inc_value();
-								}
-								if ($gameBoard[$r-1][$c]->get_mine()){
-									$gameBoard[$r][$c]->inc_value();
-								}
-								if ($gameBoard[$r-1][$c-1]->get_mine()){
-									$gameBoard[$r][$c]->inc_value();
-								}
-								if ($gameBoard[$r+1][$c]->get_mine()){
-									$gameBoard[$r][$c]->inc_value();
-								}
-								if ($gameBoard[$r+1][$c-1]->get_mine()){
-									$gameBoard[$r][$c]->inc_value();
-								}
-							}elseif($c!=0 && $c != $colCount-1){
-								
-								// Check Both
-								if ($gameBoard[$r][$c+1]->get_mine()){
-									$gameBoard[$r][$c]->inc_value();
-								}
-								if ($gameBoard[$r][$c-1]->get_mine()){
-									$gameBoard[$r][$c]->inc_value();
-								}
-								if ($gameBoard[$r-1][$c+1]->get_mine()){
-									$gameBoard[$r][$c]->inc_value();
-								}
-								if ($gameBoard[$r-1][$c-1]->get_mine()){
-									$gameBoard[$r][$c]->inc_value();
-								}
-								if ($gameBoard[$r-1][$c]->get_mine()){
-									$gameBoard[$r][$c]->inc_value();
-								}
-								if ($gameBoard[$r+1][$c]->get_mine()){
-									$gameBoard[$r][$c]->inc_value();
-								}
-								if ($gameBoard[$r+1][$c+1]->get_mine()){
-									$gameBoard[$r][$c]->inc_value();
-								}
-								if ($gameBoard[$r+1][$c-1]->get_mine()){
-									$gameBoard[$r][$c]->inc_value();
-								}
-								
-							}
-						}									
-				}// End Inner 
-			}// End Outer >
-		?>
-		<!DOCTYPE html>
-			<title>Minesweeper</title>
-			<style>
-				table {
-					border-collapse: collapse;
-				}
-
-				td, a {
-					text-align:      center;
-					width:           1em;
-					height:          1em;
-				}
-
-				a {
-					display:         block;
-					color:           black;
-					text-decoration: none;
-					font-size:       2em;
-				}
-			</style>
-			<table border="1">
-				<script src="http://code.jquery.com/jquery-1.10.2.js"></script>
+			<script src="http://code.jquery.com/jquery-1.10.2.js"></script>
 				<script src="http://code.jquery.com/ui/1.11.2/jquery-ui.js"></script>
 				<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
 			<script type="text/javascript">
@@ -364,29 +54,311 @@
 				 {
 				 	counter = 0;
 				 	localStorage.setItem("seconds",counter);
-				 }
-				 
-				 
+				 } 
 			</script>
-			<body onload='timer=setInterval("countUP()", 1000 );'>
-			Time: <div id="timer_container"></div>
-			<br>
+
+		</p>
+
 		<?php
-			// this is a test
-			for($r =0;  $r <$rowCount-1; $r++){
-				echo '<tr>';
-				for($c=0; $c<$colCount-1; $c++){
-					echo '<td>';
-					echo '<a href>.</a>';
-					/*if ($gameBoard[$r][$c]->get_mine()){
-						echo "*"; 
-					}elseif($gameBoard[$r][$c]->get_value()!=0) {
-						echo $gameBoard[$r][$c]->get_value();
-					}*/
-				}// End of Inner for 			
-			}// End of outer for 
+			session_start();
+			$user = $_SESSION["user_name"];
+			echo "Welcome ".$user;
+
+			if (isset($_POST['logout']))
+        	{
+        			session_start();
+
+        			echo '<script type="text/javascript">',
+     					'clearTimer();',
+    					 '</script>';
+					$_SESSION["user_name"] = "";
+					unset($_SESSION['user_name']);
+					clearSweeper();
+					header("Refresh: 1; url = http://cs3750juan.epizy.com/login.php");		             	
+			}
+	
+			if(isset($_POST['postcounter']))
+			{
+				$time = $_POST['postcounter'];
+				$user = $_SESSION["user_name"];
+				
+				$servername = "sql304.epizy.com";
+				$username = "epiz_23868833";
+				$password = "oZwNrrIhSLY3r";
+				$dbname =  "epiz_23868833_game";
+
+				$conn = new mysqli($servername, $username,$password,$dbname);
+
+				if($conn->connect_error)
+				{
+					die("Connection failed:". $conn->connect_error);
+				}
+				$sql = "SELECT * FROM  GameMines WHERE User = '" . $user . "'";
+				$result = $conn->query($sql);
+				if($result->num_rows > 0)
+				{
+					$row = $result->fetch_assoc();
+					$idUser = $row['ID'];
+					$query = "INSERT INTO Scores (IDName, Score) VALUES ('" . $idUser . "', '" . $time . "')"; 
+					mysqli_query($conn,$query);
+					echo $idUser." ".$time;
+					session_start();
+					$_SESSION["user_name"] = $user;
+				}
+
+			}
+		?>
+	
+		<?php
+		define('MINEGRID_WIDTH',  9);
+		define('MINEGRID_HEIGHT', 9);
+
+		define('MINESWEEPER_NOT_EXPLORED', -1);
+		define('MINESWEEPER_MINE',         -2);
+		define('MINESWEEPER_FLAGGED',      -3);
+		define('MINESWEEPER_FLAGGED_MINE', -4);
+		define('ACTIVATED_MINE',           -5);
+
+		function check_field($field) {
+		    if ($field === MINESWEEPER_MINE || $field === MINESWEEPER_FLAGGED_MINE) {
+		        return true;
+		    }
+		    else {
+		        return false;
+		    }
+		}
+
+		function explore_field($field) {
+		    if (!isset($_SESSION['minesweeper'][$field])
+		     || !in_array($_SESSION['minesweeper'][$field],
+		                  array(MINESWEEPER_NOT_EXPLORED, MINESWEEPER_FLAGGED))) {
+		        return;
+		    }
+
+		    $mines = 0;
+
+		    // Make reference to that long name
+		    $fields  = &$_SESSION['minesweeper'];
+
+
+		    // left side options
+		    if ($field % MINEGRID_WIDTH !== 1) {
+		        $mines += check_field(@$fields[$field - MINEGRID_WIDTH - 1]);
+		        $mines += check_field(@$fields[$field - 1]);
+		        $mines += check_field(@$fields[$field + MINEGRID_WIDTH - 1]);
+		    }
+
+		    // bottom and top
+		    $mines += check_field(@$fields[$field - MINEGRID_WIDTH]);
+		    $mines += check_field(@$fields[$field + MINEGRID_WIDTH]);
+
+		    // right side options
+		    if ($field % MINEGRID_WIDTH !== 0) {
+		        $mines += check_field(@$fields[$field - MINEGRID_WIDTH + 1]);
+		        $mines += check_field(@$fields[$field + 1]);
+		        $mines += check_field(@$fields[$field + MINEGRID_WIDTH + 1]);
+		    }
+
+		    $fields[$field] = $mines;
+
+		    if ($mines === 0) {
+		        if ($field % MINEGRID_WIDTH !== 1) {
+		            explore_field($field - MINEGRID_WIDTH - 1);
+		            explore_field($field - 1);
+		            explore_field($field + MINEGRID_WIDTH - 1);
+		        }
+
+		        explore_field($field - MINEGRID_WIDTH);
+		        explore_field($field + MINEGRID_WIDTH);
+
+		        if ($field % MINEGRID_WIDTH !== 0) {
+		            explore_field($field - MINEGRID_WIDTH + 1);
+		            explore_field($field + 1);
+		            explore_field($field + MINEGRID_WIDTH + 1);
+		        }
+		    }
+		}
+
+		session_start(); // will start session storage
+
+		if (!isset($_SESSION['minesweeper'])) {
+		    // Fill grid with not explored tiles
+		    $_SESSION['minesweeper'] = array_fill(1,
+		                                         MINEGRID_WIDTH * MINEGRID_HEIGHT,
+		                                         MINESWEEPER_NOT_EXPLORED);
+
+		    $number_of_mines = 10;
+
+		    // generate mines randomly
+		    $random_keys = array_rand($_SESSION['minesweeper'], $number_of_mines);
+
+		    foreach ($random_keys as $key) {
+		        $_SESSION['minesweeper'][$key] = MINESWEEPER_MINE;
+		    }
+
+		    // to make calculations shorter use SESSION variable to store the result
+		    $_SESSION['numberofmines'] = $number_of_mines;
+		}
+
+		if (isset($_GET['explore'])) {
+		    if(isset($_SESSION['minesweeper'][$_GET['explore']])) {
+		        switch ($_SESSION['minesweeper'][$_GET['explore']]) {
+		            case MINESWEEPER_NOT_EXPLORED:
+		                explore_field($_GET['explore']);
+		                break;
+		            case MINESWEEPER_MINE:
+		                $lost = 1;
+		                $_SESSION['minesweeper'][$_GET['explore']] = ACTIVATED_MINE;
+		                break;
+		            default:
+		                // The tile was discovered already. Ignore it.
+		                break;
+		        }
+		    }
+		    else {
+		        die('Tile doesn\'t exist.');
+		    }
+		}
+		elseif (isset($_GET['flag'])) {
+		    if(isset($_SESSION['minesweeper'][$_GET['flag']])) {
+		        $tile = &$_SESSION['minesweeper'][$_GET['flag']];
+		        switch ($tile) {
+		            case MINESWEEPER_NOT_EXPLORED:
+		                $tile = MINESWEEPER_FLAGGED;
+		                break;
+		            case MINESWEEPER_MINE:
+		                $tile = MINESWEEPER_FLAGGED_MINE;
+		                break;
+		            case MINESWEEPER_FLAGGED:
+		                $tile = MINESWEEPER_NOT_EXPLORED;
+		                break;
+		            case MINESWEEPER_FLAGGED_MINE:
+		                $tile = MINESWEEPER_MINE;
+		                break;
+		            default:
+		                // This tile shouldn't be flagged. Ignore it.
+		                break;
+		        }
+		    }
+		    else {
+		        die('Tile doesn\'t exist.');
+		    }
+		}
+
+		// Check if the player won...
+		if (!in_array(MINESWEEPER_NOT_EXPLORED, $_SESSION['minesweeper'])
+		 && !in_array(MINESWEEPER_FLAGGED,      $_SESSION['minesweeper'])) {
+		    $won = true;
+		}
+		?>
+		<style>
+		table {
+		    border-collapse: collapse;
+		}
+
+		td, a {
+		    text-align:      center;
+		    width:           1em;
+		    height:          1em;
+		}
+
+		a {
+		    display:         block;
+		    color:           black;
+		    text-decoration: none;
+		    font-size:       2em;
+		}
+		</style>
+		<script>
+		function flag(number, e) {
+		    if (e.which === 2 || e.which === 3) {
+		        location = '?flag=' + number;
+		        return false;
+		    }
+		}
+		</script>
+		<table border="1">
+		<?php
+		$mine_copy = $_SESSION['minesweeper'];
+
+		for ($x = 1; $x <= MINEGRID_HEIGHT; $x++) {
+		    echo '<tr>';
+		    for ($y = 1; $y <= MINEGRID_WIDTH; $y++) {
+		        echo '<td>';
+
+		        $number = array_shift($mine_copy);
+		        switch ($number) {
+		            case MINESWEEPER_FLAGGED:
+		            case MINESWEEPER_FLAGGED_MINE:
+		                if (!empty($lost) || !empty($won)) {
+		                    if ($number === MINESWEEPER_FLAGGED_MINE) {
+		                        echo '<a>*</a>';
+		                    }
+		                    else {
+		                        echo '<a>.</a>';
+		                    }
+		                }
+		                else {
+		                    echo '<a href=# onmousedown="return flag(',
+		                         ($x - 1) * MINEGRID_WIDTH + $y,
+		                         ',event)" oncontextmenu="return false">?</a>';
+		                }
+		                break;
+		            case ACTIVATED_MINE:
+		                echo '<a>:(</a>';
+		                break;
+		            case MINESWEEPER_MINE:
+		            case MINESWEEPER_NOT_EXPLORED:
+		                if (!empty($lost)) {
+		                    if ($number === MINESWEEPER_MINE) {
+		                        echo '<a>*</a>';
+		                    }
+		                    else {
+		                        echo '<a>.</a>';
+		                    }
+		                }
+		                elseif (!empty($won)) {
+		                    echo '<a>*</a>';
+		                }
+		                else {
+		                    echo '<a href="?explore=',
+		                         ($x - 1) * MINEGRID_WIDTH + $y,
+		                         '" onmousedown="return flag(',
+		                         ($x - 1) * MINEGRID_WIDTH + $y,
+		                         ',event)" oncontextmenu="return false">.</a>';
+		                }
+		                break;
+		            case 0:
+		                echo '<a></a>';
+		                break;
+		            default:
+		                echo '<a>', $number, '</a>';
+		                break;
+		        }
+		    }
+		}
 		?>
 		</table>
+		<?php
+		if (!empty($lost)) {
+		    unset($_SESSION['minesweeper']);
+		    echo '<p>You lost1 :(. <a href="?">Reboot?</a>';
+		    echo '<script type="text/javascript">',
+     					'stopCounter();',
+    					 '</script>';
+    		header("Refresh: 1; url = http://cs3750juan.epizy.com/playGame.php");	
+		}
+		elseif (!empty($won)) {
+		    unset($_SESSION['minesweeper']);
+		    echo '<p>Congratulations. You won :).';
+		}
 
-	</body>
+		function clearSweeper()
+		{
+			unset($_SESSION['minesweeper']);
+		}
+
+		?>
+		
+		</body>
 </html>
